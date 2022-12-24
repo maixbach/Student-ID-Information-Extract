44bdb1648b02d6507e75a86d5bbe84623b575239
Anh Tong <tong.v.anh@icloud.com>
2022-12-24 12:20:58 +0700

    update

---
+++ config.py
@@ -0,0 +1,9 @@
+vocab = '''aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcC
+            dDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊ
+            jJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢ
+            pPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ
+            0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ Ð–‘“’'''
+
+weights = 'https://drive.google.com/file/d/1qkG85VwXNKNdER3EkNf4gmXor7edZ7aX'
+
+
--- ocr.py
+++ ocr.py
@@ -2,18 +2,19 @@ import numpy as np
 import torch
 import cv2
 
-from vietocr.tool.translate import build_model, translate, translate_beam_search, predict
-from vietocr.tool.utils import download_weights
+from vietocr.tool.translate import translate, translate_beam_search, resize
 from vietocr.tool.predictor import Predictor
 from vietocr.tool.config import Cfg
+import config as cf
 
 
 class NewPredictor(Predictor):
     def predict(self, img, return_prob=False):
         # process input
         h, w = img.shape[:2]
-        new_w, image_height = resize(w, h, config['dataset']['image_height'], config['dataset']['image_min_width'],
-                                     config['dataset']['image_max_width'])
+        new_w, image_height = resize(w, h, self.config['dataset']['image_height'],
+                                     self.config['dataset']['image_min_width'],
+                                     self.config['dataset']['image_max_width'])
         img = cv2.resize(img, (new_w, image_height))
 
         img = img.transpose(2, 0, 1)
@@ -41,15 +42,18 @@ class NewPredictor(Predictor):
             return s
 
 
-def ocr(img):
-    config = Cfg.load_config_from_name('vgg_transformer')
-    # config['weights'] = './weights/transformerocr.pth'
-    config['vocab'] = '''aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ0123456789!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ Ð–‘“’'''
-    config['weights'] = 'https://drive.google.com/file/d/1qkG85VwXNKNdER3EkNf4gmXor7edZ7aX'
-    config['cnn']['pretrained'] = False
-    config['device'] = 'cuda:0'
-    config['predictor']['beamsearch'] = False
+class Cfg(Cfg):
+    def load_update_config(self, name):
+        config = self.load_config_from_name(name)
+        config['vocab'] = cf.vocab
+        config['weights'] = cf.weights
+        config['cnn']['pretrained'] = False
+        config['device'] = 'cuda:0'
+        config['predictor']['beamsearch'] = False
 
+
+def ocr(img):
+    config = Cfg.load_update_config('vgg_transformer')
     detector = NewPredictor(config)
     s = detector.predict(img)
     return s
